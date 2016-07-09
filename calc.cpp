52d8ded8bcbd55cc08c91c1b27964507f290f2c8
Sameer Barule <Sameer Barule>
2016-07-09 14:13:01 +0530

    version1 with add and sub

---
+++ calc.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+int main()
+{
+	int a,b,c,ch;
+	cout<<"enter 2 nos.";
+	cin>>a>>b;
+	
+    switch(ch)
+	{
+		case 1 : c=a+b;
+				cout<<"Addition is : "<<c;
+				break;
+				
+		case 2 : c=a-b;
+				cout<<"Sub is : "<<c;
+				break;
+				
+	
+	}
+	return 0;
+}
